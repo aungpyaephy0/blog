472ea83864cdd58c084406eda77a033c76ddc28f
AungPyaePhyo <pyaea978@gmail.com>
2023-11-07 02:24:32 +0630

    Add New Article Form and  check validate form data

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use App\Models\Article;
 use Illuminate\Http\Request;
 
@@ -10,16 +11,52 @@ class ArticleController extends Controller
     {
         $data = Article::latest()->paginate(5);
         return view('articles.index', [
-        'articles' => $data
+            'articles' => $data
         ]);
     }
 
-    public function detail ($id)
+    public function detail($id)
     {
         $data = Article::find($id);
- return view('articles.detail', [
- 'article' => $data
- ]);
+        return view('articles.detail', ['article' => $data]);
+    }
+
+    public function add()
+    {
+
+        $data = [
+            [ "id" => 1, "name" => "News" ],
+            [ "id" => 2, "name" => "Tech" ],
+            ];
+            return view('articles.add', [
+            'categories' => $data
+            ]);
+    }
+
+    public function create()
+    {
+        $validator = validator(request()->all(), [
+            'title' => 'required',
+            'body' => 'required',
+            'category_id' => 'required',
+            ]);
+            if($validator->fails()) {
+            return back()->withErrors($validator);
+            }
+        $article = new Article;
+        $article->title = request()->title;
+        $article->body = request()->body;
+        $article->category_id = request()->category_id;
+        $article->save();
+        return redirect('/articles');
 
     }
+    public function delete($id)
+    {
+        $article = Article::find($id);
+        $article->delete();
+        return redirect('/articles')->with('info', 'Article deleted successfully');
+    }
+
 }
+
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ArticleController;
 use App\Http\Controllers\Product\ProductController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -20,12 +21,12 @@ Route::get('/', function () {
 
 Route::get('/', [ArticleController::class, 'index']);
 Route::get('/articles', [ArticleController::class, 'index']);
-Route::get('/articles/detail/{id}', [
-ArticleController::class,
-'detail'
-]);
-
+Route::get('/articles/detail/{id}', [ArticleController::class, 'detail']);
+Route::get('/articles/delete/{id}', [ArticleController::class, 'delete']);
 Route::get('/products', [ProductController::class, 'index']);
+Route::post('/articles/add', [ArticleController::class, 'create']);
+Route::get('/articles/add', [ArticleController::class, 'add']);
+Route::get('/articles/delete/{id}', [ArticleController::class,'delete']);
 
 
 // Auth::routes();
