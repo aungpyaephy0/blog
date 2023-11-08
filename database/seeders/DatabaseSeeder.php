ad69717c5d142712b9b0130889801db10a0be586
AungPyaePhyo <pyaea978@gmail.com>
2023-11-08 18:41:45 +0630

    Prepare database model between articles and comment

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Article;
+use App\Models\Category;
+use App\Models\Comment;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,7 +15,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        \App\Models\Article::factory()->count(20)->create();
+        Article::factory()->count(20)->create();
+        Category::factory()->count(5)->create();
+        Comment::factory()->count(40)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
